0cdb517fb28d13a60b333a9cf1a4e7a8ad3bb47a
azhul <azhul@griddynamics.com>
2024-11-21 15:38:12 +0100

    metrics top 5

--- src/main/java/org/example/config/KafkaStreamConfig.java
+++ src/main/java/org/example/config/KafkaStreamConfig.java
@@ -7,6 +7,7 @@ import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.Materialized;
 import org.apache.kafka.streams.kstream.Produced;
 import org.apache.kafka.streams.kstream.TimeWindows;
 import org.example.model.CommitMessage;
@@ -15,7 +16,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafkaStreams;
 
 import java.time.Duration;
+import java.util.Comparator;
 import java.util.Properties;
+import java.util.TreeMap;
 
 @Configuration
 @EnableKafkaStreams
@@ -62,6 +65,38 @@ public class KafkaStreamConfig {
                 })
                 .to("commits-per-language-output", Produced.with(Serdes.String(), Serdes.String()));
 
+
+        // Metric 4: Top 5 committers
+        commitStream
+                .mapValues(this::convertToCommitMessage)
+                .groupBy((key, commitMessage) -> commitMessage.getUsername())
+                .count(Materialized.as("commit-count-store"))
+                .toStream()
+                .to("commit-count-output");
+
+        KStream<String, Long> commitCountStream = streamsBuilder.stream("commit-count-output");
+
+        commitCountStream
+                .groupByKey()
+                .aggregate(() -> new TreeMap<Long, String>(Comparator.reverseOrder()),
+                        (aggKey, newValue, aggValue) -> {
+                            String committer = aggKey;
+                            Long commitCount = newValue;
+                            aggValue.put(commitCount, committer);
+                            if (aggValue.size() > 5) {
+                                aggValue.pollLastEntry();
+                            }
+                            return aggValue;
+                        }
+                )
+                .toStream()
+                .map((aggKey, committersMap) -> {
+                    String topCommittersMessage = "Top 5 committers: " +
+                            String.join(", ", committersMap.values());
+                    return new KeyValue<>("Top 5 Committers", topCommittersMessage);
+                })
+                .to("top-committers-output", Produced.with(Serdes.String(), Serdes.String()));
+
         Properties config = streamsConfig();
         config.put(StreamsConfig.PROCESSING_GUARANTEE_CONFIG, StreamsConfig.EXACTLY_ONCE);
         return new KafkaStreams(streamsBuilder.build(), config);
@@ -74,6 +109,7 @@ public class KafkaStreamConfig {
         config.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         return config;
     }
+
     private CommitMessage convertToCommitMessage(String value) {
         try {
             return objectMapper.readValue(value, CommitMessage.class);
